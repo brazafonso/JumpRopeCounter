5a201391f69f5be79a8e98997a5e167d26150232
brazafonso <61985783+brazafonso@users.noreply.github.com>
2023-05-12 20:51:30 +0100

    login e logout (google)
    apenas em debug mode

--- mobile/app/src/main/java/com/example/jumpropecounter/User/User.kt
+++ mobile/app/src/main/java/com/example/jumpropecounter/User/User.kt
@@ -1,8 +1,26 @@
 package com.example.jumpropecounter.User
 
-class User (username:String,password:String){
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import com.google.firebase.auth.FirebaseAuth
+import java.io.ByteArrayOutputStream
+import java.io.ObjectOutputStream
+import java.util.prefs.Preferences
+import kotlin.io.path.Path
+import kotlin.io.path.createDirectories
+import kotlin.io.path.createFile
+import kotlin.io.path.writeBytes
+
+class User (username:String){
     private var username = username
     private var total_jumps = 0
+
+
+    fun sign_out(){
+        FirebaseAuth.getInstance().signOut()
+    }
+
+
 
 
 }
