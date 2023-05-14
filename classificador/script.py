55bcf5aebfff4a04efc87ba29e5c0d90a393bf7d
rsaSantos <rs.elsaint@gmail.com>
2023-05-14 19:43:03 +0100

    first model: not that bad

--- classificador/script.py
+++ classificador/script.py
@@ -8,6 +8,7 @@ cred = credentials.Certificate("sa-g4-a91ed-ecd15449f098.json")
 firebase_admin.initialize_app(cred, {'storageBucket': 'sa-g4-a91ed.appspot.com'})
 
 folder_path = 'data'
+final_size = (128, 256)
 
 bucket = storage.bucket()
 
@@ -40,15 +41,55 @@ def upload_images():
             blob = bucket.blob(folder_path + '/' + filename)
             blob.upload_from_filename(os.path.join(folder_path, filename))
 
-def check_size():
+def check_size(folder_name, width=128, height=256):
     all_same_size = True
-    for filename in os.listdir(folder_path):
+    path = os.path.join(folder_path, folder_name)
+    for filename in os.listdir(path):
         if filename.endswith(".jpg"):
-            im = Image.open(os.path.join(folder_path, filename))
-            if im.size[0] != 480 or im.size[1] != 640:
-                print("Check size of image {}!".format(filename))
+            im = Image.open(os.path.join(path, filename))
+            img_width, img_height = im.size
+            if img_width != width or img_height != height:
+                print("Image {} is not {}x{} pixels.".format(filename, width, height))
                 all_same_size = False
     
     if all_same_size:
-        print("All images are 480x640 pixels.")
+        print("All images are {}x{} pixels.".format(width, height))
+
+# Name of the image is "1234567.jpg" (numbers followed by .jpg)
+# Change the name to "1234567_jumping.jpg" (numbers followed by _jumping.jpg) or "1234567_not_jumping.jpg"
+# depending on the folder the image is in (jumping or not_jumping)
+def rename_images(folder_name):
+    path = os.path.join(folder_path, folder_name)
+    for filename in os.listdir(path):
+        if filename.endswith(".jpg"):
+            print("Renaming image {}...".format(filename))
+            os.rename(os.path.join(path, filename), os.path.join(path, filename[:-4] + '_' + folder_name + '.jpg'))
+
+
+# Crop borders of images
+def crop_images(folder_name):
+    path = os.path.join(folder_path, folder_name)
+    for filename in os.listdir(path):
+        if filename.endswith(".jpg"):
+            print("Cropping image {}...".format(filename))
+            im = Image.open(os.path.join(path, filename))
+
+            # Get dimensions
+            width, height = im.size
+
+            # Crop 80 pixels from left, 80 pixels from right, 0 pixels from top and 100 pixels from bottom
+            crop_left, crop_right, crop_top, crop_bottom = 80, width - 80, 0, height - 100
+            im = im.crop((crop_left, crop_top, crop_right, crop_bottom))
+
+            im.save(os.path.join(path, filename))
+
+# Resize images to 128x256 pixels
+def resize_images(folder_name):
+    path = os.path.join(folder_path, folder_name)
+    for filename in os.listdir(path):
+        if filename.endswith(".jpg"):
+            print("Resizing image {}...".format(filename))
+            im = Image.open(os.path.join(path, filename))
+            im = im.resize(final_size)
+            im.save(os.path.join(path, filename))
 
